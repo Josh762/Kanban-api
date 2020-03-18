0b788402c90d097a73474ea45e46ca8f9fa84443
Hunter Redding <hunterred50@gmail.com>
2020-03-18 13:52:30 -0500

    board stuff

--- src/controllers/BoardController.js
+++ src/controllers/BoardController.js
@@ -1,15 +1,17 @@
-//
-// import Controller from './Controller';
-// import BoardService from "./../services/BoardService";
-// import Board from './../models/Post';
-//
-// const boardService = new BoardService(
-//     new Board().getInstance()
-// );
-//
-// class BoardController extends Controller {
-//
-//     constructor(service) {
-//         super(service);
-//     }
-// }
+
+import Controller from './Controller';
+import BoardService from "./../services/BoardService";
+import Board from './../models/Board';
+
+const boardService = new BoardService(
+    new Board().getInstance()
+);
+
+class BoardController extends Controller {
+
+    constructor(service) {
+        super(service);
+    }
+}
+
+export default new BoardController(boardService)
--- src/models/Board.js
+++ src/models/Board.js
@@ -6,7 +6,19 @@ class Board {
 
     initSchema() {
         const schema = new Schema({
-
+            title: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: false,
+            },
+            columns: {
+                type: [String],
+                required: true,
+                default: []
+            }
         })
     }
 }
