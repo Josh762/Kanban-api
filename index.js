03aabbf95a4f934d10e55ea8e0a85c238a0c23a4
Josh762 <joshbrewster762@gmail.com>
2020-03-18 13:15:04 -0500

    Start

---
+++ index.js
@@ -0,0 +1,38 @@
+//
+// import express from 'express';
+// import './api/controllers/boardsAPI'
+//
+// async function startServer() {
+//     const app = express();
+//
+//     /**
+//      * A little hack here
+//      * Import/Export can only be used in 'top-level code'
+//      * Well, at least in node 10 without babel and at the time of writing
+//      * So we are using good old require.
+//      **/
+//     await require('./loaders').default({ expressApp: app });
+//     const port = 3000; // TODO needs to be in a config
+//     app.listen(port, err => {
+//         if (err) {
+//             // process.exit(1);
+//             console.error("Server Failed to Start", err);
+//             return;
+//         }
+//         console.info(`
+//       ################################################
+//       🛡️  Server listening on port: ${port} 🛡️
+//       ################################################
+//     `);
+//     });
+// }
+//
+// startServer();
+
+import server from './config/server';
+import './config/database';
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`);
+});
