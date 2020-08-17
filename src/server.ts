1b7b4b0f6722ea77d482517b8c36ea5f5cd59811
Josh Brewster <joshbrewster762@gmail.com>
2020-08-16 21:11:50 -0500

    adding envalid package

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,13 @@ import 'dotenv/config';
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
 import validateEnv from './utils/validateEnv';
-
+import middleware from "./middleware";
 validateEnv();
-
-
 
 const controllers:any[] = [new AuthenticationController()]
 
 try {
-    const app = new App(controllers)
+    const app = new App(controllers, middleware)
     app.listen();
 } catch(e) {
     console.error(`The server failed to start.\n`,e)
